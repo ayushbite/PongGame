0bc824e907f41426c7e2be8e5a6445b01daf71a6
ayush <ayushbite@gmail.com>
2022-05-25 20:26:07 +0530

    refactoring code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include "raylib.h"
 
-int main(void)
-{
+
+struct Ball {
+    float ball_xaxis, ball_yaxis;
+    float ball_speed_xaxis, ball_speed_yaxis;
+    float ball_raidus;
+
+    void DrawBAll() {
+        DrawCircle((int) ball_xaxis, (int) ball_yaxis, ball_raidus, WHITE);
+
+    }
+
+
+};
+
+int main(void) {
     // Initialization
     //--------------------------------------------------------------------------------------
     const int screenWidth = 500;
@@ -9,50 +22,60 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-                   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
 
 /*    vsync hint*/
     SetWindowState(FLAG_VSYNC_HINT);
-    float ball_xaxis = GetScreenWidth()/2.0f;
-    float ball_yaxis = GetScreenHeight()/2.0f;
-    float ball_raidus = 10;
-    float ball_speed_xaxis = 10;
-    float ball_speed_yaxis = 10;
+
+    Ball ball;//struct of ball
+    ball.ball_xaxis = GetScreenWidth() / 2.0f;
+    ball.ball_yaxis = GetScreenHeight() / 2.0f;
+    ball.ball_raidus = 10;
+    ball.ball_speed_xaxis = 50;
+    ball.ball_speed_yaxis = 500;
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
 //        before drarwing update the ball axis with speed
-            ball_xaxis += ball_speed_xaxis * GetFrameTime();
-            ball_yaxis += ball_speed_yaxis * GetFrameTime();
+        ball.ball_xaxis += ball.ball_speed_xaxis * GetFrameTime();
+        ball.ball_yaxis += ball.ball_speed_yaxis * GetFrameTime();
 
+//         writing a condition when the ball is on to the end of the screen then boucing of the ball
+
+//            when the ball goes to the top of the screen
+        if (ball.ball_yaxis < 0) {
+            ball.ball_yaxis = 0;
+            ball.ball_speed_yaxis *= -1;
+
+
+        }
+
+//        when ball is bottom of the screen
+        if (ball.ball_yaxis > GetScreenHeight()) {
+            ball.ball_yaxis = GetScreenHeight();
+            ball.ball_speed_yaxis *= -1;
+        }
 
 
         BeginDrawing();
 
         ClearBackground(BLACK);
-        DrawFPS(10,10);
+        DrawFPS(10, 10);
 
 //        Drawing circle on to the middle of the screen
-        DrawCircle((int)ball_xaxis,(int)ball_yaxis,ball_raidus,WHITE);
+        ball.DrawBAll();
+
 //        Drawing a rectangle on the left side as an counter attact rod
 //        and making the y-axis subtracting to the half of the height of the rectangle to to appear as to the center
         int height_of_rectangle = 100;
-        DrawRectangle(10,GetScreenHeight()/2 - (height_of_rectangle/2),5,height_of_rectangle,WHITE);
-        DrawRectangle(GetScreenWidth() - 10-5,GetScreenHeight()/2 - (height_of_rectangle/2),5,height_of_rectangle,WHITE);
-
-
-
-
-
+        DrawRectangle(10, GetScreenHeight() / 2 - (height_of_rectangle / 2), 5, height_of_rectangle, WHITE);
+        DrawRectangle(GetScreenWidth() - 10 - 5, GetScreenHeight() / 2 - (height_of_rectangle / 2), 5,
+                      height_of_rectangle, WHITE);
 
 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
+
     //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
